03f01833465229a0603d28c4c636ab6d9058d14a
fernandobouchet <fernandobouchet@gmail.com>
2025-03-19 23:43:38 -0300

    feat: add 'period-tabs' component

---
+++ components/period-tabs.tsx
@@ -0,0 +1,38 @@
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
+interface Props {
+  periods: period[];
+}
+
+const PeriodsTabs = ({ periods }: Props) => {
+  console.log(periods);
+  return (
+    <Tabs defaultValue="1" className="w-full flex items-center">
+      <TabsList className="flex w-full p-0 h-14 border-none rounded-3xl bg-sidebar">
+        {periods.map((period) => (
+          <TabsTrigger
+            key={period.id}
+            value={period.order.toString()}
+            className="rounded-3xl h-full text-base w-full data-[state=active]:bg-accent data-[state=active]:text-accent-foreground data-[state=active]:hover:bg-accent/90 hover:bg-muted hover:text-foreground cursor-pointer"
+          >
+            {period.order}
+          </TabsTrigger>
+        ))}
+      </TabsList>
+      {periods.map((period) => (
+        <TabsContent
+          key={period.id}
+          value={period.order.toString()}
+          className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-5"
+        >
+          {period.courses?.map((course) => {
+            console.log(course);
+            return <p key={course.id}>{course.name}</p>;
+          })}
+        </TabsContent>
+      ))}
+    </Tabs>
+  );
+};
+
+export { PeriodsTabs };
