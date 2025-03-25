23c3b5460e0f56127455459c51732fa73e7b3e61
fernandobouchet <fernandobouchet@gmail.com>
2025-03-24 22:08:20 -0300

    feat: update 'seed' data

--- server/db/seeds/career_courses.ts
+++ server/db/seeds/career_courses.ts
@@ -59,4 +59,33 @@ export const careersCoursesSeed = [
   { id: crypto.randomUUID(), careerId: 1, courseId: 47, periodId: null },
   { id: crypto.randomUUID(), careerId: 1, courseId: 48, periodId: null },
   { id: crypto.randomUUID(), careerId: 1, courseId: 49, periodId: null },
+
+  // 1er Cuatrimestre
+  { id: crypto.randomUUID(), careerId: 2, courseId: 101, periodId: 12 }, // Matemática para Informática I
+  { id: crypto.randomUUID(), careerId: 2, courseId: 102, periodId: 12 }, // Introducción a Lógica y Problemas Computacionales
+  { id: crypto.randomUUID(), careerId: 2, courseId: 103, periodId: 12 }, // Organización de Computadoras I
+  { id: crypto.randomUUID(), careerId: 2, courseId: 4, periodId: 12 }, // Nuevos Entornos y Lenguajes (Lic)
+
+  // 2do Cuatrimestre
+  { id: crypto.randomUUID(), careerId: 2, courseId: 104, periodId: 13 }, // Taller de Lenguajes de Marcado y Tecnologías Web (Correlativa: 102)
+  { id: crypto.randomUUID(), careerId: 2, courseId: 105, periodId: 13 }, // Programación Estructurada (Correlativa: 102)
+  { id: crypto.randomUUID(), careerId: 2, courseId: 106, periodId: 13 }, // Matemática para Informática II (Correlativa: 101)
+  { id: crypto.randomUUID(), careerId: 2, courseId: 8, periodId: 13 }, // Inglés I (Lic)
+
+  // 3er Cuatrimestre
+  { id: crypto.randomUUID(), careerId: 2, courseId: 7, periodId: 14 }, // Bases de Datos (Lic) (Correlativa: 106)
+  { id: crypto.randomUUID(), careerId: 2, courseId: 6, periodId: 14 }, // Programación con Objetos I (Lic) (Correlativa: 105)
+  { id: crypto.randomUUID(), careerId: 2, courseId: 5, periodId: 14 }, // Estructuras de Datos (Lic) (Correlativa: 105)
+
+  // 4to Cuatrimestre
+  { id: crypto.randomUUID(), careerId: 2, courseId: 107, periodId: 15 }, // Materia UNAHUR
+  { id: crypto.randomUUID(), careerId: 2, courseId: 10, periodId: 15 }, // Programación con Objetos II (Lic) (Correlativa: 8)
+  { id: crypto.randomUUID(), careerId: 2, courseId: 13, periodId: 15 }, // Programación Funcional (Lic, Electiva I) (Correlativas: 105, 9)
+  { id: crypto.randomUUID(), careerId: 2, courseId: 11, periodId: 15 }, // Redes de Computadoras (Correlativa: 103)
+  { id: crypto.randomUUID(), careerId: 2, courseId: 23, periodId: 15 }, // Inglés II (Lic) (Correlativa: 6)
+
+  // 5to Cuatrimestre
+  { id: crypto.randomUUID(), careerId: 2, courseId: 14, periodId: 16 }, // Construcción de Interfaces de Usuario (Lic) (Correlativa: 10)
+  { id: crypto.randomUUID(), careerId: 2, courseId: 16, periodId: 16 }, // Estrategias de Persistencia (Lic) (Correlativas: 10, 7)
+  { id: crypto.randomUUID(), careerId: 2, courseId: 20, periodId: 16 }, // Elementos de Ingeniería de Software (Lic) (Correlativa: 10)
 ];
--- server/db/seeds/periods.ts
+++ server/db/seeds/periods.ts
@@ -8,32 +8,38 @@ export const periodsSeed = [
   { id: 5, order: 5, careerId: 1 },
 
   // Tecnicatura en Informática (6 períodos)
-  { id: 7, order: 1, careerId: 2 },
-  { id: 8, order: 2, careerId: 2 },
-  { id: 9, order: 3, careerId: 2 },
-  { id: 10, order: 4, careerId: 2 },
+  { id: 6, order: 1, careerId: 2 },
+  { id: 7, order: 2, careerId: 2 },
+  { id: 8, order: 3, careerId: 2 },
+  { id: 9, order: 4, careerId: 2 },
+  { id: 10, order: 5, careerId: 2 },
+  { id: 11, order: 6, careerId: 2 },
 
   // Tecnicatura en Programación (5 períodos)
-  { id: 11, order: 1, careerId: 3 },
-  { id: 12, order: 2, careerId: 3 },
-  { id: 13, order: 3, careerId: 3 },
-  { id: 14, order: 4, careerId: 3 },
+  { id: 12, order: 1, careerId: 3 },
+  { id: 13, order: 2, careerId: 3 },
+  { id: 14, order: 3, careerId: 3 },
+  { id: 15, order: 4, careerId: 3 },
+  { id: 16, order: 5, careerId: 3 },
 
   // Tecnicatura en Redes (5 períodos)
-  { id: 15, order: 1, careerId: 4 },
-  { id: 16, order: 2, careerId: 4 },
-  { id: 17, order: 3, careerId: 4 },
-  { id: 18, order: 4, careerId: 4 },
+  { id: 17, order: 1, careerId: 4 },
+  { id: 18, order: 2, careerId: 4 },
+  { id: 19, order: 3, careerId: 4 },
+  { id: 20, order: 4, careerId: 4 },
+  { id: 21, order: 5, careerId: 4 },
 
   // Tecnicatura en IA (5 períodos)
-  { id: 19, order: 1, careerId: 5 },
-  { id: 20, order: 2, careerId: 5 },
-  { id: 21, order: 3, careerId: 5 },
-  { id: 22, order: 4, careerId: 5 },
+  { id: 22, order: 1, careerId: 5 },
+  { id: 23, order: 2, careerId: 5 },
+  { id: 24, order: 3, careerId: 5 },
+  { id: 25, order: 4, careerId: 5 },
+  { id: 26, order: 5, careerId: 5 },
 
   // Tecnicatura en Videojuegos (5 períodos)
-  { id: 23, order: 1, careerId: 6 },
-  { id: 24, order: 2, careerId: 6 },
-  { id: 25, order: 3, careerId: 6 },
-  { id: 26, order: 4, careerId: 6 },
+  { id: 27, order: 1, careerId: 6 },
+  { id: 28, order: 2, careerId: 6 },
+  { id: 29, order: 3, careerId: 6 },
+  { id: 30, order: 4, careerId: 6 },
+  { id: 31, order: 5, careerId: 6 },
 ];
