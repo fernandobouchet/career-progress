af50f9952f8f369e5c54bde3eca4346abd376389
fernandobouchet <fernandobouchet@gmail.com>
2025-03-27 23:08:23 -0300

    feat(trpc): add functionality to change course status

--- server/api/root.ts
+++ server/api/root.ts
@@ -1,5 +1,6 @@
 import { createCallerFactory, createTRPCRouter } from "@/server/api/trpc";
 import { careerRouter } from "./routers/career";
+import { userRouter } from "./routers/user";
 
 /**
  * This is the primary router for your server.
@@ -8,6 +9,7 @@ import { careerRouter } from "./routers/career";
  */
 export const appRouter = createTRPCRouter({
   career: careerRouter,
+  user: userRouter,
 });
 
 // export type definition of API
---
+++ server/api/routers/user.ts
@@ -0,0 +1,60 @@
+import { createTRPCRouter, protectedProcedure } from "@/server/api/trpc";
+import { usersCourses } from "@/server/db/schema";
+import { statusKeys } from "@/types/constants";
+import { eq } from "drizzle-orm";
+import { z } from "zod";
+
+export const userRouter = createTRPCRouter({
+  updateCourseProgress: protectedProcedure
+    .input(
+      z.object({
+        courseId: z.number(),
+        status: z.enum(statusKeys),
+        qualification: z.number().min(4).max(10).nullable(),
+      })
+    )
+    .mutation(async ({ input: { courseId, status, qualification }, ctx }) => {
+      const user = await ctx.session?.user;
+
+      if (!user) return null;
+
+      const course = await ctx.db.query.courses.findFirst({
+        where: (courses, { eq }) => eq(courses.id, courseId),
+      });
+
+      if (!course) return null;
+
+      const userCourse = await ctx.db.query.usersCourses.findFirst({
+        where: (uc, { and, eq }) =>
+          and(eq(uc.userId, user.id), eq(uc.courseId, courseId)),
+      });
+
+      if (status === "PENDIENTE") {
+        if (userCourse) {
+          await ctx.db
+            .delete(usersCourses)
+            .where(eq(usersCourses.id, userCourse.id));
+        }
+        return { success: true };
+      }
+
+      if (!userCourse) {
+        await ctx.db.insert(usersCourses).values({
+          userId: user.id,
+          courseId: courseId,
+          status,
+          qualification,
+        });
+      } else {
+        await ctx.db
+          .update(usersCourses)
+          .set({
+            status,
+            qualification,
+          })
+          .where(eq(usersCourses.id, userCourse.id));
+      }
+
+      return { success: true };
+    }),
+});
