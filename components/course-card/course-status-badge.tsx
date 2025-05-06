c5c41cf82bee6d893560cf0666cc90fdd61307b3
fernandobouchet <fernandobouchet@gmail.com>
2025-05-05 23:26:01 -0300

    feat: update 'course-status' badge

--- components/course-card/course-status-badge.tsx
+++ components/course-card/course-status-badge.tsx
@@ -1,6 +1,7 @@
 import { useUserData } from "@/context/user-data-context";
 import { Badge } from "../ui/badge";
 import { Skeleton } from "../ui/skeleton";
+import { useSession } from "next-auth/react";
 
 interface Props {
   status: keyof typeof StatusEnum | undefined;
@@ -8,7 +9,13 @@ interface Props {
 }
 
 const CourseStatusBadge = ({ className, status }: Props) => {
+  const { status: authStatus } = useSession();
+
   const { isLoading } = useUserData();
+
+  if (authStatus === "unauthenticated") {
+    return <div className="w-20 h-6 rounded-full ml-auto"></div>;
+  }
 
   if (isLoading) {
     return <Skeleton className="w-20 h-6 rounded-full ml-auto" />;
